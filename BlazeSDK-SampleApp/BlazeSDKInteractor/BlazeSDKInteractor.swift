4d14a8b0781bba5732f10b3f894ade719347127f
DorZafrir <dor@monkeytech.co.il>
2023-10-09 19:00:04 +0300

    update version to 0.1.0
    add container tab

--- BlazeSDK-SampleApp/BlazeSDKInteractor/BlazeSDKInteractor.swift
+++ BlazeSDK-SampleApp/BlazeSDKInteractor/BlazeSDKInteractor.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 import BlazeSDK
+import UIKit
 
 final class BlazeSDKInteractor {
     
     struct Constants {
         static let defaultCacheSize = 500
         static let apiKey = "[API - KEY]"
-        static let storiesRowLabel = "live-stories"
-        static let storiesGridLabel = "top-stories"
-        static let momentsRowLabel = "moments"
-        static let momentsGridLabel = "moments"
+        static let storiesRowLabel = "[stories label]"
+        static let storiesGridLabel = "[stories label]"
+        static let momentsRowLabel = "[moments label]"
+        static let momentsGridLabel = "[moments label]"
+        static let momentsContainerTabLabel = "[moments Container label]"
     }
     
     static var shared: BlazeSDKInteractor = BlazeSDKInteractor()
@@ -30,6 +32,9 @@ final class BlazeSDKInteractor {
     private(set) var storiesGridWidgetLabel: String = Constants.storiesGridLabel
     private(set) var momentsRowWidgetLabel: String = Constants.momentsRowLabel
     private(set) var momentsGridWidgetLabel: String = Constants.momentsGridLabel
+    private(set) var momentsContainerTabLabel: String = Constants.momentsContainerTabLabel
+    
+    private var momentsContainersDic: [String: BlazeMomentsPlayerContainer] = [:]
     
     func initBlazeSDK() {
         Blaze.shared.initialize(apiKey: apiKey, cachingSize: cachingSize, prefetchingPolicy: prefetchLevel) { [weak self] error in
@@ -41,6 +46,17 @@ final class BlazeSDKInteractor {
     
     func dismissCurrentPlayer() {
         blazeSdk.dismissCurrentPlayer()
+    }
+    
+    func generateMomentsTab(containerId: String, dataSourceType: BlazeDataSourceType, momentsAppearance: BlazeMomentsAppearance? = nil, delegate: BlazePlayerContainerDelegate? = nil) {
+        let playerContainer = BlazeMomentsPlayerContainer(dataSourceType: dataSourceType, containerDelegate: delegate, containerIdentifier: containerId, appearance: momentsAppearance)
+        momentsContainersDic[containerId] = playerContainer
+    }
+    
+    
+    func playMomentsInContainer(containerId: String, containerVC: UIViewController, containerView: UIView? = nil) {
+        guard let playerContainer = momentsContainersDic[containerId] else { return }
+        playerContainer.startPlaying(in: containerVC, containerView: containerView)
     }
     
     private func handleBlazeSdkInitalError(for error: BlazeError) {
---
+++ BlazeSDK-SampleApp/Main/TabBar/MomentsContainerTab/MomentsContainerViewController.swift
@@ -0,0 +1,128 @@
+//
+//  MomentsContainerViewController.swift
+//  BlazeSDK-SampleApp
+//
+//  Created by Dor Zafrir on 09/10/2023.
+//
+
+import UIKit
+import BlazeSDK
+import Combine
+
+class MomentsContainerViewController: UIViewController {
+    
+    @IBOutlet weak var viewLoader: UIActivityIndicatorView!
+    @IBOutlet weak var lblEmptyState: UILabel!
+    
+    private var loadingState: CurrentValueSubject<Bool, Never> = .init(false)
+    private var emptyState: CurrentValueSubject<Bool, Never> = .init(false)
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    struct Constants {
+        static let tabId = "MomentsContainerTab"
+    }
+        
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupMomentsTab()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubscriptions()
+        playMoments()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    private func setupMomentsTab() {
+        let dataSourceType: BlazeDataSourceType = .labels(.singleLabel(BlazeSDKInteractor.shared.momentsContainerTabLabel))
+        var appearance = BlazeMomentsAppearance()
+        appearance.buttons.exit.isVisible = false
+        appearance.buttons.exit.isVisibleForAds = false
+        appearance.buttons.mute.isVisible = false
+        appearance.buttons.mute.isVisibleForAds = false
+        appearance.seekBarStyle.isVisible = false
+        appearance.headerGradient.isVisible = false
+        BlazeSDKInteractor.shared.generateMomentsTab(containerId: Constants.tabId, dataSourceType: dataSourceType, momentsAppearance: appearance, delegate: self)
+    }
+    
+    private func playMoments() {
+        BlazeSDKInteractor.shared.playMomentsInContainer(containerId: Constants.tabId, containerVC: self)
+    }
+    
+    private func addSubscriptions() {
+        addLoadingStateSubscription()
+        addEmptyStateSubscription()
+    }
+    
+    private func addLoadingStateSubscription() {
+        loadingState
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] isLoading in
+                self?.handleLoadingState(isLoading: isLoading)
+            }
+            .store(in: &subscriptions)
+    }
+    
+    private func handleLoadingState(isLoading: Bool) {
+        isLoading ? startLoading() : endLoading()
+    }
+    
+    private func startLoading() {
+        viewLoader.startAnimating()
+        viewLoader.isHidden = false
+    }
+    
+    private func endLoading() {
+        viewLoader.stopAnimating()
+        viewLoader.isHidden = true
+    }
+    
+    private func addEmptyStateSubscription() {
+        emptyState
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] isEmpty in
+                self?.handleEmptyState(isEmpty: isEmpty)
+            }
+            .store(in: &subscriptions)
+    }
+    
+    private func handleEmptyState(isEmpty: Bool) {
+        lblEmptyState.isHidden = !isEmpty
+    }
+
+    
+}
+
+extension MomentsContainerViewController: BlazePlayerContainerDelegate {
+    func onDataLoadStarted(containerId: String) {
+        loadingState.send(true)
+        print("onDataLoadStarted delegate, containerId: \(containerId)")
+    }
+    
+    func onDataLoadComplete(containerId: String, itemsCount: Int, result: BlazeSDK.BlazeResult) {
+        loadingState.send(false)
+        emptyState.send(itemsCount == 0)
+        switch result {
+        case .success():
+            print("onDataLoadComplete delegate, containerId: \(containerId), number of items: \(itemsCount)")
+        case .failure(let error): print("onDataLoadComplete with error delegate, containerId: \(containerId), error: \(error.errorMessage)")
+        }
+    }
+    
+    func onContainedPlayerDismissed(containerId: String) {
+        print("onContainedPlayerDismissed delegate, containerId: \(containerId)")
+    }
+    
+    func onTriggerCTA(containerId: String, actionType: String, actionParam: String) -> Bool {
+        return false
+    }
+    
+    
+    
+
+}
